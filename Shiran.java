2df5bc56bd01035136f4391daf4f1cc610e118cb
shiranAharoni <shiran5725@gmail.com>
2024-12-08 20:36:02 +0200

    shiran change

--- Shiran.java
+++ Shiran.java
@@ -1,8 +1,8 @@
-public class Shiran{
-	public static void main (string[] args){
-		for (int i=1 ; i<=10 ; i++){
-			system.out.println("MY NAME IS shiran "+i);
-			}
-		}
-	}
-	
+
+public class Shiran {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("shiran print!! " + i);
+        }
+    }
+}
